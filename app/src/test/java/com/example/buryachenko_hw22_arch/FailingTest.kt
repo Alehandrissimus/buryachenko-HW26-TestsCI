39efe451983a98f73042fc8861627517336d33ad
Alehandrissimus <buryachenko10@gmail.com>
2021-03-13 19:16:37 +0200

    tests

---
+++ app/src/test/java/com/example/buryachenko_hw22_arch/FailingTest.kt
@@ -0,0 +1,13 @@
+package com.example.buryachenko_hw22_arch
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class FailingTest {
+
+    @Test
+    fun `some test`(a: Int = 10) {
+        assertEquals(a, 5)
+    }
+
+}

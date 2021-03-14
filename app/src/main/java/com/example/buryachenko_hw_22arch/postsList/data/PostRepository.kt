719b85376712c264fe01c8fe54fb739532f643a6
Alehandrissimus <buryachenko10@gmail.com>
2021-03-14 15:47:23 +0200

    Detekt config + various fixes

--- app/src/main/java/com/example/buryachenko_hw_22arch/postsList/data/PostRepository.kt
+++ app/src/main/java/com/example/buryachenko_hw_22arch/postsList/data/PostRepository.kt
@@ -1,13 +1,13 @@
 package com.example.buryachenko_hw_22arch.postsList.data
 
-import android.util.Log
 import com.example.buryachenko_hw_22arch.postsList.data.api.PostService
 import com.example.buryachenko_hw_22arch.postsList.data.mappers.PostMapper
 import com.example.buryachenko_hw_22arch.postsList.data.models.Post
 import com.example.buryachenko_hw_22arch.postsList.data.models.PostModel
 import com.example.buryachenko_hw_22arch.postsList.data.room.PostsDatabase
 import com.example.buryachenko_hw_22arch.tools.Result
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class PostRepository @Inject constructor(
@@ -22,7 +22,7 @@ class PostRepository @Inject constructor(
 
     fun subscribeForInfo(): Flow<Result<List<PostModel>, String>> {
         return database.postsDao().getAllPosts()
-            .map { it.sortedWith(compareBy( { !it.isCreatedByUser }, { it.isCreatedByUser } )) }
+            .map { it.sortedWith(compareBy({ !it.isCreatedByUser }, { it.isCreatedByUser })) }
             .map { postMapper.mapping(Result.success(it)) }
     }
 
